5393663bd669ba49da3689dff0ef518500cf7709
Dave Compton <davecompton7@gmail.com>
2017-05-26 17:03:39 +0000

    better use of comparing

--- Main.hs
+++ Main.hs
@@ -1,15 +1,6 @@
-import Data.List
+import Data.List(transpose, nub, sort, maximumBy)
 import Data.Ord (comparing)
 import Data.Char (ord)
-
-englishFrequencies = 
-     [ 0.08167, 0.01492, 0.02782, 0.04253, 
-       0.12702, 0.02228, 0.02015, 0.06094, 
-       0.06966, 0.00153, 0.00772, 0.04025, 
-       0.02406, 0.06749, 0.07507, 0.01929, 
-       0.00095, 0.05987, 0.06327, 0.09056, 
-       0.02758, 0.00978, 0.02360, 0.00150, 
-       0.01974, 0.00074 ] 
 
 crypt = "\
     \MOMUD EKAPV TQEFM OEVHP AJMII CDCTI FGYAG JSPXY ALUYM NSMYH\
@@ -31,6 +22,15 @@ crypt = "\
     \FWAML ZZRXJ EKAHV FASMU LVVUT TGK\
     \"
 
+englishFrequencies = 
+    [ 0.08167, 0.01492, 0.02782, 0.04253, 
+      0.12702, 0.02228, 0.02015, 0.06094, 
+      0.06966, 0.00153, 0.00772, 0.04025, 
+      0.02406, 0.06749, 0.07507, 0.01929, 
+      0.00095, 0.05987, 0.06327, 0.09056, 
+      0.02758, 0.00978, 0.02360, 0.00150, 
+      0.01974, 0.00074 ] 
+
 average :: [Float] -> Float
 average as = sum as / fromIntegral (length as)
 
@@ -51,8 +51,8 @@ distribute as n = transpose $ breakup n as
 
 -- The probability that members of a pair of characters taken randomly
 -- from a given string are equal.
-coincProb :: String -> Float
-coincProb str = 
+coincidence :: String -> Float
+coincidence str = 
     let charCounts = fmap (countChar str) (nub $ sort str)
         strln = length str
         d = fromIntegral $ strln * (strln - 1)
@@ -64,23 +64,30 @@ coincProb str =
 -- The correlation increases artificially for smaller
 -- pieces/longer keys, so weigh against them a little
 rate :: [String] -> Float
-rate d =  average (fmap coincProb d) - fromIntegral (length d) / 3000.0 
+rate d =  average (fmap coincidence d) - fromIntegral (length d) / 3000.0 
+
+-- Multiply elements of lists together and add up the results.
+dot :: [Float] -> [Float] -> Float
+dot l0 l1 = sum $ zipWith (*) l0 l1
+
+-- Given two lists of floats, shift one of them by the number of
+-- characters indicated by letter and then 'dot' them together.
+shiftAndDot :: [Float] -> [Float] -> Char -> Float
+shiftAndDot l0 l1 ltr = dot l0 (drop (ord ltr - ord 'A') (cycle l1))  
 
 -- find decoding offset that results in best match 
 -- between actual char frequencies and expected frequencies
 getKeyChar :: [Float] -> String -> Char
-getKeyChar expectedFrequencies encodedString =
-    let actualFrequencies = fmap (fromIntegral . countChar encodedString) ['A'..'Z']
-        dots = fmap (\ltr -> (ltr, sum $ zipWith (*) expectedFrequencies (drop (ord ltr - ord 'A') (cycle actualFrequencies))) ) ['A'..'Z']
-    in fst $ maximumBy (comparing snd) dots
+getKeyChar expected sample =
+    let actual = fmap (fromIntegral . countChar sample) ['A'..'Z']
+    in maximumBy (comparing $ shiftAndDot expected actual) ['A'..'Z']
 
 main = do
     let cr = filter (/=' ') crypt
         -- Assume that if there are less than 20 characters
         -- per column, the key's too long to guess
         distributions = fmap (distribute cr) [1..length cr `div` 20]
-        ratedDistributions = fmap (\d -> (d, rate d)) distributions
-        bestDistribution = fst $ maximumBy (comparing snd) ratedDistributions
+        bestDistribution = maximumBy (comparing rate) distributions
         key = fmap (getKeyChar englishFrequencies) bestDistribution
         alphaSum a b = cycle ['A'..'Z'] !! (26 + (ord b - ord a))
     mapM_ putStrLn [key, zipWith alphaSum (cycle key) cr]
